442ada7d5f6a832281fa34458529886e437f4e8d
Minsoo Kang <alstn38@naver.com>
2025-04-18 14:49:42 +0900

    feat: TrendView 구현

---
+++ TrendBit2/TrendBit2/Present/Trend/View/Main/TrendView.swift
@@ -0,0 +1,76 @@
+//
+//  TrendView.swift
+//  TrendBit2
+//
+//  Created by 강민수 on 4/18/25.
+//
+
+import SwiftUI
+
+struct TrendView: View {
+    
+    private let gridRows: [GridItem] = Array(
+        repeating: GridItem(.fixed(60), spacing: 0),
+        count: 3
+    )
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .leading, spacing: 32) {
+
+                    /// My Favorite
+                    SectionHeaderView("My Favorite")
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 16) {
+                            ForEach(sampleFavorites) { coin in
+                                FavoriteCoinCardView(coin: coin)
+                            }
+                        }
+                        .padding(.horizontal, 16)
+                    }
+
+                    /// Top 15 Coin
+                    SectionHeaderView("Top15 Coin")
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHGrid(rows: gridRows, alignment: .top, spacing: 0) {
+                            ForEach(sampleTopCoins) { coin in
+                                RankCoinRowView(coin: coin)
+                                    .frame(width: 300)
+                                    .padding(.horizontal)
+                            }
+                        }
+                        .padding(.horizontal, 16)
+                    }
+                    .frame(height: 60 * 3 + 12 * 2)
+
+                    /// Top 7 NFT (스켈레톤)
+                    SectionHeaderView("Top7 NFT")
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHGrid(rows: gridRows, alignment: .top, spacing: 0) {
+                            ForEach(sampleTopCoins) { coin in
+                                RankCoinRowView(coin: coin)
+                                    .frame(width: 300)
+                                    .padding(.horizontal)
+                            }
+                        }
+                        .padding(.horizontal, 16)
+                    }
+                    .frame(height: 60 * 3 + 12 * 2)
+                }
+                .padding(.top, 24)
+            }
+            .navigationTitle("TrendBit2 Coin")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    ProfileAvatarView()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    TrendView()
+}

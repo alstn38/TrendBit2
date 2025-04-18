037895b719a8623dbe581968cc3ba7bd2b34fb40
Minsoo Kang <alstn38@naver.com>
2025-04-18 14:49:31 +0900

    feat: FavoriteCoinCardView 구현

---
+++ TrendBit2/TrendBit2/Present/Trend/View/Sub/FavoriteCoinCardView.swift
@@ -0,0 +1,58 @@
+//
+//  FavoriteCoinCardView.swift
+//  TrendBit2
+//
+//  Created by 강민수 on 4/18/25.
+//
+
+import SwiftUI
+
+struct FavoriteCoinCardView: View {
+    
+    let coin: FavoriteCoinEntity
+    private var coinColor: Color {
+        if coin.change > 0 {
+            return .red
+        } else if coin.change < 0 {
+            return .blue
+        } else {
+            return .black
+        }
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            HStack(spacing: 12) {
+                Image(systemName: coin.iconName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(.orange)
+                    .padding(8)
+                    .background(Color.white)
+                    .clipShape(Circle())
+
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(coin.name)
+                        .textStyle(font: .headline, weight: .bold)
+                    Text(coin.ticker)
+                        .textStyle(font: .subheadline, color: .secondary)
+                }
+            }
+
+            Text(coin.price)
+                .textStyle(font: .title3, weight: .semibold)
+            
+            Text(String(format: "%+.2f%%", coin.change))
+                .textStyle(
+                    font: .subheadline,
+                    color: coinColor,
+                    weight: .bold
+                )
+        }
+        .padding()
+        .frame(width: 260, alignment: .leading)
+        .background(Color(.systemGray6))
+        .clipShape(RoundedRectangle(cornerRadius: 24, style: .continuous))
+    }
+}

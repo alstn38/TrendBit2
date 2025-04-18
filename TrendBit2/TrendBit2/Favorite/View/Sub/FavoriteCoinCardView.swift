54ee6221079b02748ef583ed2b6de491b8bf877a
Minsoo Kang <alstn38@naver.com>
2025-04-18 15:37:09 +0900

    feat: FavoriteCoinCardView 구현

---
+++ TrendBit2/TrendBit2/Favorite/View/Sub/FavoriteCoinCardView.swift
@@ -0,0 +1,64 @@
+//
+//  FavoriteCoinCardView.swift
+//  TrendBit2
+//
+//  Created by 강민수 on 4/18/25.
+//
+
+import SwiftUI
+
+struct FavoriteCoinCardView: View {
+    
+    let coin: FavoriteCoinEntity
+    private var coinColor: Color {
+        if coin.change > 0 {
+            return .red
+        } else if coin.change < 0 {
+            return .blue
+        } else {
+            return .black
+        }
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            HStack(spacing: 8) {
+                Image(systemName: coin.iconName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 28, height: 28)
+                    .foregroundColor(.orange)
+
+                VStack(alignment: .leading, spacing: 2) {
+                    Text(coin.name)
+                        .textStyle(font: .headline, weight: .bold)
+                    Text(coin.ticker)
+                        .textStyle(font: .subheadline, color: .secondary)
+                }
+            }
+            
+            HStack {
+                Spacer()
+                Text(coin.price)
+                    .textStyle(font: .title3, weight: .bold)
+            }
+            .padding(.top, 20)
+            
+            HStack {
+                Spacer()
+                Text(String(format: "%+.2f%%", coin.change))
+                    .textStyle(font: .footnote, color: coinColor, weight: .bold)
+                    .foregroundColor(coinColor)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 4)
+                    .background(coinColor.opacity(0.1))
+                    .clipShape(RoundedRectangle(cornerRadius: 6, style: .continuous))
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 3)
+    }
+}

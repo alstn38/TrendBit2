518d541dc6a7bef085f9984b8e18a462300bd896
Minsoo Kang <alstn38@naver.com>
2025-04-18 13:27:14 +0900

    init: TrendBit2 project 생성

---
+++ TrendBit2/TrendBit2/App/TrendBit2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrendBit2App.swift
+//  TrendBit2
+//
+//  Created by 강민수 on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TrendBit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

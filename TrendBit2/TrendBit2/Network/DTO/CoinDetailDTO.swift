adb7056666469a4b575de918ed9a73fbdf13a660
Minsoo Kang <alstn38@naver.com>
2025-04-18 20:52:45 +0900

    feat: CoinDetailDTO Model 정의

---
+++ TrendBit2/TrendBit2/Network/DTO/CoinDetailDTO.swift
@@ -0,0 +1,84 @@
+//
+//  CoinDetailDTO.swift
+//  TrendBit2
+//
+//  Created by 강민수 on 4/18/25.
+//
+
+import Foundation
+
+struct CoinDetailDTO: Decodable {
+    let id: String
+    let symbol: String
+    let name: String
+    let image: String
+    let currentPrice: Double
+    let marketCap: Double
+    let marketCapRank: Int
+    let fullyDilutedValuation: Double?
+    let totalVolume: Double
+    let high24H: Double?
+    let low24H: Double?
+    let priceChange24H: Double?
+    let priceChangePercentage24H: Double?
+    let marketCapChange24H: Double?
+    let marketCapChangePercentage24H: Double?
+    let circulatingSupply: Double?
+    let totalSupply: Double?
+    let maxSupply: Double?
+    let ath: Double
+    let athChangePercentage: Double
+    let athDate: String
+    let atl: Double
+    let atlChangePercentage: Double
+    let atlDate: String
+    let lastUpdated: String
+    let sparklineIn7D: SparklineIn7D
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case symbol
+        case name
+        case image
+        case currentPrice = "current_price"
+        case marketCap = "market_cap"
+        case marketCapRank = "market_cap_rank"
+        case fullyDilutedValuation = "fully_diluted_valuation"
+        case totalVolume = "total_volume"
+        case high24H = "high_24h"
+        case low24H = "low_24h"
+        case priceChange24H = "price_change_24h"
+        case priceChangePercentage24H = "price_change_percentage_24h"
+        case marketCapChange24H = "market_cap_change_24h"
+        case marketCapChangePercentage24H = "market_cap_change_percentage_24h"
+        case circulatingSupply = "circulating_supply"
+        case totalSupply = "total_supply"
+        case maxSupply = "max_supply"
+        case ath
+        case athChangePercentage = "ath_change_percentage"
+        case athDate = "ath_date"
+        case atl
+        case atlChangePercentage = "atl_change_percentage"
+        case atlDate = "atl_date"
+        case lastUpdated = "last_updated"
+        case sparklineIn7D = "sparkline_in_7d"
+    }
+}
+
+struct SparklineIn7D: Decodable {
+    let price: [Double]
+}
+
+// MARK: - To Entity Method
+extension CoinDetailDTO {
+    
+    func toFavoriteEntity() -> FavoriteCoinEntity {
+        return FavoriteCoinEntity(
+            name: name,
+            ticker: symbol.uppercased(),
+            price: NumberFormatterManager.shared.pointNumberString(from: currentPrice),
+            change: priceChangePercentage24H ?? 0.0,
+            iconName: image
+        )
+    }
+}

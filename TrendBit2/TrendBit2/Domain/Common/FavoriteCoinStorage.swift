2848519690c30ec698d8206803b5c037cc67d7cb
Minsoo Kang <alstn38@naver.com>
2025-04-19 15:32:37 +0900

    feat: Coin 즐겨찾기 Toast 기능 추가

--- TrendBit2/TrendBit2/Domain/Common/FavoriteCoinStorage.swift
+++ TrendBit2/TrendBit2/Domain/Common/FavoriteCoinStorage.swift
@@ -20,23 +20,58 @@ final class FavoriteCoinStorage {
         return favorites[coinID] != nil
     }
 
-    func setFavorite(_ isFavorite: Bool, for coinID: String) {
+    func setFavorite(_ isFavorite: Bool, for coinID: String) -> FavoriteState {
         if isFavorite {
-            favorites[coinID] = true
+            if favorites.count < 10 {
+                favorites[coinID] = true
+                ToastManager.shared.show(message: FavoriteState.successFavorite.message)
+                return .successFavorite
+            } else {
+                ToastManager.shared.show(message: FavoriteState.failFavorite.message)
+                return .failFavorite
+            }
         } else {
             favorites.removeValue(forKey: coinID)
+            ToastManager.shared.show(message: FavoriteState.deleteFavorite.message)
+            return .deleteFavorite
         }
     }
 
-    func toggleFavorite(for coinID: String) {
+    func toggleFavorite(for coinID: String) -> FavoriteState {
         if favorites[coinID] != nil {
             favorites.removeValue(forKey: coinID)
+            ToastManager.shared.show(message: FavoriteState.deleteFavorite.message)
+            return .deleteFavorite
         } else {
-            favorites[coinID] = true
+            if favorites.count < 10 {
+                favorites[coinID] = true
+                ToastManager.shared.show(message: FavoriteState.successFavorite.message)
+                return .successFavorite
+            } else {
+                ToastManager.shared.show(message: FavoriteState.failFavorite.message)
+                return .failFavorite
+            }
         }
     }
 
     func allFavorites() -> [String] {
         return Array(favorites.keys)
+    }
+    
+    enum FavoriteState {
+        case successFavorite
+        case failFavorite
+        case deleteFavorite
+        
+        var message: String {
+            switch self {
+            case .successFavorite:
+                return "즐겨찾기에 성공적으로 추가되었습니다."
+            case .failFavorite:
+                return "즐겨찾기는 최대 10개까지만 가능합니다."
+            case .deleteFavorite:
+                return "즐겨찾기에서 제거되었습니다."
+            }
+        }
     }
 }

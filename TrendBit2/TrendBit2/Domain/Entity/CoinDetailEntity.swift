678c44d8618a460f61c8942083d94b76a0a85641
Minsoo Kang <alstn38@naver.com>
2025-04-19 13:56:43 +0900

    chore: CoinDetailEntity paramerter 수정

--- TrendBit2/TrendBit2/Domain/Entity/CoinDetailEntity.swift
+++ TrendBit2/TrendBit2/Domain/Entity/CoinDetailEntity.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-struct CoinDetailEntity {
+struct CoinDetailEntity: Identifiable {
+    let id: String
     let name: String
     let iconName: String
     let price: String
@@ -18,21 +19,5 @@ struct CoinDetailEntity {
     let recordLow: String
     let changePrices: [Double]
     let lastUpdated: String
-    var isFavorite: Bool
+    var isFavorite: Bool = false
 }
-
-let sampleDetail = CoinDetailEntity(
-    name: "Solana",
-    iconName: "ethereum.sign.circle",
-    price: "₩69,234,245",
-    change: +3.22,
-    high: "₩69,234,243",
-    low: "₩69,234,245",
-    recordHigh: "₩2,869,234,243",
-    recordLow: "₩6,364",
-    changePrices: [
-        50, 52, 53, 55, 54, 57, 60, 59, 63, 67, 65, 70, 68, 64, 60, 58, 55, 56, 58, 61, 64, 66, 65, 63, 62
-    ],
-    lastUpdated: "2/21 11:53:50 업데이트",
-    isFavorite: true
-)

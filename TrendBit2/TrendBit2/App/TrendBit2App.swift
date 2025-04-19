dec4be2417e88ddd83890a5e0c9a12c26440025c
Minsoo Kang <alstn38@naver.com>
2025-04-19 15:31:48 +0900

    feat: AppDelegate Toast 기능 추가

--- TrendBit2/TrendBit2/App/TrendBit2App.swift
+++ TrendBit2/TrendBit2/App/TrendBit2App.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct TrendBit2App: App {
+    
+    @StateObject private var toastManager = ToastManager.shared
+    
     var body: some Scene {
         WindowGroup {
             TabBarView()
+                .overlay(
+                    VStack {
+                        if toastManager.isPresented {
+                            ToastView(message: toastManager.message)
+                                .animation(.easeInOut, value: toastManager.isPresented)
+                                .zIndex(1)
+                        }
+                        Spacer()
+                    }
+                )
         }
     }
 }
